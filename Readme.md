83249587d6a7a09772ac4015d8f4ab2db3e2426a
Sundeep Chand <sundeep.chand2001@gmail.com>
2021-05-26 17:18:11 +0530

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,55 @@
+# Underwater Image colour Enhancement
+
+OpenCV Python implementation of the image enhancement method from the paper [`N. Carlevaris-Bianco, A. Mohan, and R. M. Eustice. Initial results in underwater single image dehazing. In Proc.
+MTS/IEEE OCEANS, 2010.`](https://deepblue.lib.umich.edu/bitstream/handle/2027.42/86035/ncarlevaris-3.pdf?sequence=1&isAllowed=y)
+
+# Results
+
+###  Image1
+Input
+
+![fish1 input](./image/ocean_input/fish.jpeg)
+
+Result
+
+![fish1 input](./image/ocean_output/fish.jpeg)
+
+
+### Image 2
+Input
+
+![fish2 input](./image/ocean_input/fish2.jpg)
+
+Result
+
+![fish2 input](./image/ocean_output/fish2.jpg)
+
+
+### Image 3
+Input
+
+![gate input](./image/ocean_input/gate.png)
+
+Result
+
+![gate input](./image/ocean_output/gate.png)
+
+
+### Image 4
+Input
+
+![path_marker input](./image/ocean_input/path_marker.png)
+
+Result
+
+![path_marker input](./image/ocean_output/path_marker.png)
+
+
+### Image 5
+Input
+
+![table input](./image/ocean_input/table.png)
+
+Result
+
+![table input](./image/ocean_output/table.png)
---
+++ ocean_enhance.py
@@ -0,0 +1,113 @@
+import numpy as np
+import cv2
+
+
+def resize(img, perc=0.5):
+    width = int(img.shape[1] * perc)
+    height = int(img.shape[0] * perc)
+    resized = cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)
+    return resized
+
+
+def dark_channel(img, sz=15):
+    b, g, r = cv2.split(img)
+    dc = r - cv2.max(b, g)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (sz, sz))
+    dark = cv2.erode(dc, kernel)
+    return dark
+
+
+def airlight(img, dc):
+    [h, w] = img.shape[:2]
+    imgsz = h * w
+    imgvec = img.reshape(imgsz, 3)
+    dcvec = dc.reshape(imgsz)
+
+    indices = dcvec.argsort()
+    return imgvec[indices[0]]
+
+
+def transmission_estimate(dc):
+    te = dc + (1 - np.max(dc))
+    return te
+
+
+def guided_filter(img, p, r, eps):
+    mean_I = cv2.boxFilter(img, cv2.CV_64F, (r, r))
+    mean_p = cv2.boxFilter(p, cv2.CV_64F, (r, r))
+    mean_Ip = cv2.boxFilter(img * p, cv2.CV_64F, (r, r))
+    cov_Ip = mean_Ip - mean_I * mean_p
+
+    mean_II = cv2.boxFilter(img * img, cv2.CV_64F, (r, r))
+    var_I = mean_II - mean_I * mean_I
+
+    a = cov_Ip / (var_I + eps)
+    b = mean_p - a * mean_I
+
+    mean_a = cv2.boxFilter(a, cv2.CV_64F, (r, r))
+    mean_b = cv2.boxFilter(b, cv2.CV_64F, (r, r))
+
+    q = mean_a * img + mean_b
+    return q
+
+
+def transmission_refine(img, et):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = np.float64(gray) / 255
+    r = 60
+    eps = 0.0001
+    t = guided_filter(gray, et, r, eps)
+    return t
+
+
+def recover(img, t, A):
+    res = np.empty(img.shape, img.dtype)
+    for index in range(0, 3):
+        res[:, :, index] = (img[:, :, index] - A[index]) / t + A[index]
+
+    return res
+
+
+def normalize_image(img):
+    img = img - img.min()
+    img = img / img.max() * 255
+    img = np.uint8(img)
+    return img
+
+
+def dummy(x):
+    pass
+
+
+if __name__ == '__main__':
+    filename = 'table.png'
+    scale = 1
+
+    src = cv2.imread(f'image/ocean_input/{filename}')
+    src = resize(src, scale)
+
+    img = src.astype('float64') / 255
+
+    cv2.namedWindow('controls')
+    cv2.createTrackbar('ksize', 'controls', 1, 600, dummy)
+
+    while True:
+        ksize = int(cv2.getTrackbarPos('ksize', 'controls')) + 1
+
+        dc = dark_channel(img, ksize)
+        te = transmission_estimate(dc)
+        tr = transmission_refine(src, te)
+        A = airlight(img, tr)
+        result = recover(img, tr, A)
+        result = normalize_image(result)
+
+        cv2.imshow('Transmission estimate', te)
+        cv2.imshow('Transmission refined', tr)
+        cv2.imshow('Input', img)
+        cv2.imshow('Result', result)
+
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+
+    cv2.imwrite(f'image/ocean_output/{filename}', result)
